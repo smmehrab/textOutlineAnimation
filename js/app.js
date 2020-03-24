7d8497581df366779faebd69ede5d251c633283b
‘S.M.Mehrabul <mehrab.24csedu.001@gmail.com>
2020-03-25 04:42:35 +0600

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const logo = document.querySelectorAll("#logo path");
+
+// To find the length of each path
+// for (let i = 0; i < logo.length; i++) {
+//     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+// }
